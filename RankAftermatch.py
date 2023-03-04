ea03fb22b3d1675b40427119cd147c9c11a2e6c2
Jinyang Liu <lovejoyyoung@gmail.com>
2023-03-04 00:34:13 -0500

    premierleague

---
+++ RankAftermatch.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+import json
+import csv
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_TOKEN')
+url1 = "https://api.football-data.org/v2/competitions/PL/matches?season=2021"
+url2 = "https://api.football-data.org/v2/competitions/PL/teams?season=2021"
+headers = {"X-Auth-Token": api_key}
+
+response1 = requests.get(url1, headers=headers)
+response2 = requests.get(url2, headers=headers)
+
+data1 = json.loads(response1.text)
+data2 = json.loads(response2.text)
+
+matches = pd.DataFrame(data1["matches"])
+teams = pd.DataFrame(data2["teams"])
+
+# Get initial points and rankings for each team
+points = pd.DataFrame({
+    "team_id": teams["id"],
+    "team_name": teams["name"],
+    "points": 0,
+    "position": 0
+})
+
+
+with open("21-22PL_RankAftermatch.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(["ID", "Position", "Team Name", "Points"])
+    
+# Calculate points and rankings for each match
+    for _, match in matches.iterrows():
+        home_team_id = match["homeTeam"]["id"]
+        away_team_id = match["awayTeam"]["id"]
+        home_team_name = match["homeTeam"]["name"]
+        away_team_name = match["awayTeam"]["name"]
+        home_score = match["score"]["fullTime"]["homeTeam"]
+        away_score = match["score"]["fullTime"]["awayTeam"]
+        
+        home_score = home_score if home_score is not None else 0
+        away_score = away_score if away_score is not None else 0
+        
+        # Calculate points
+        if int(home_score) > int(away_score):
+            points.loc[points["team_id"] == home_team_id, "points"] += 3
+        elif home_score < away_score:
+            points.loc[points["team_id"] == away_team_id, "points"] += 3
+        else:
+            points.loc[points["team_id"] == home_team_id, "points"] += 1
+            points.loc[points["team_id"] == away_team_id, "points"] += 1
+
+        # Calculate rankings
+        points = points.sort_values(
+            by=["points", "team_id"], ascending=[False, True])
+        points["position"] = points["points"].rank(method="dense", ascending=False)
+
+        writer.writerow("")
+        writer.writerow([f"{home_team_name} {home_score} - {away_score} {away_team_name}"])
+        writer.writerow([points[["position", "team_id", "team_name", "points"]]])
+        
---
+++ TeamAfterturn.py
@@ -0,0 +1,72 @@
+import requests
+import csv
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_TOKEN')
+url = "https://api.football-data.org/v2/competitions/PL/matches?season=2021"
+headers = {"X-Auth-Token": api_key}
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+
+team_points = {}
+team_games_played = {}
+
+tot_fc_points = 0
+tot_fc_rank = 0
+
+# Save the data list
+data_list = []
+
+for match in data["matches"]:
+    # Get match data
+    home_team = match["homeTeam"]["name"]
+    away_team = match["awayTeam"]["name"]
+    home_score = match["score"]["fullTime"]["homeTeam"]
+    away_score = match["score"]["fullTime"]["awayTeam"]
+
+    home_score = home_score if home_score is not None else 0
+    away_score = away_score if away_score is not None else 0
+
+    # Calculate Scores
+    if home_score > away_score:
+        home_points = 3
+        away_points = 0
+    elif home_score == away_score:
+        home_points = 1
+        away_points = 1
+    else:
+        home_points = 0
+        away_points = 3
+
+    # Calculate Home/Away team points and game played
+    if home_team not in team_points:
+        team_points[home_team] = 0
+        team_games_played[home_team] = 0
+    if away_team not in team_points:
+        team_points[away_team] = 0
+        team_games_played[away_team] = 0
+
+    team_points[home_team] += home_points
+    team_points[away_team] += away_points
+    team_games_played[home_team] += 1
+    team_games_played[away_team] += 1
+
+    # if fav team finished match then update data
+    if "Tottenham Hotspur FC" in [home_team, away_team]:
+        tot_fc_points = team_points["Tottenham Hotspur FC"]
+        tot_fc_rank = sorted(team_points.items(), key=lambda x: x[1], reverse=True).index(
+            ("Tottenham Hotspur FC", tot_fc_points)) + 1
+
+    # add data into list
+    data_list.append([match['matchday'], tot_fc_rank, tot_fc_points])
+
+# Write data into csv files
+with open('ToT_FC_DATA.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Round', 'Rank', 'Points'])
+    for row in data_list:
+        writer.writerow(row)
